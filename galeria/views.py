bc89c43f98bc072f2659c4a4cec1b5efed644f53
Puppyroni <puppers2022pepperoni@gmail.com>
2024-03-13 09:20:56 +0000

    Dynamic Elements Setup

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from galeria.models import Image
 
 def index(request):
-    return render(request, 'index.html') 
+    
+    set_image = Image.objects.all()
+    
+    return render(request, 'index.html', {'cards': set_image})
 
 def imagem_page(request):
-    return render(request, 'imagem.html') 
+    return render(request, 'imagem.html')
